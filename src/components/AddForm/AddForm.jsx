493ff9bf69168e41fefd7364a105650b14456202
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-12-16 22:56:42 +0200

    fix

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -5,29 +5,29 @@ export const AddForm = ({ input, submit, value }) => {
   return (
     <section className={s.addForm}>
       <form className={s.form}>
-        <label className={s.label}>Name</label>
+        <label className={s.label}>Name:</label>
         <input
           className={s.input}
-          onChange={input}
           type="text"
           name="name"
           value={value.name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          onChange={input}
         />
       </form>
       <form className={s.form}>
-        <label className={s.label}>Tel</label>
+        <label className={s.label}>Tel:</label>
         <input
           className={s.input}
-          onChange={input}
           type="tel"
           name="number"
           value={value.number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          onChange={input}
         />
       </form>
       <button className={s.button} type="submit" onClick={submit}>
@@ -38,7 +38,7 @@ export const AddForm = ({ input, submit, value }) => {
 };
 
 AddForm.propTypes = {
-  input: PropTypes.func,
-  submit: PropTypes.func,
-  value: PropTypes.object,
+  input: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired,
+  value: PropTypes.object.isRequired,
 };
