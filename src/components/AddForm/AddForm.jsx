a7fb9f937a8b1fe50d7e75a830e6f5068feec072
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-12-16 21:22:01 +0200

    init

---
+++ src/components/AddForm/AddForm.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import s from '../AddForm/AddForm.module.css';
+
+export const AddForm = ({ input, submit, value }) => {
+  return (
+    <section className={s.addForm}>
+      <form className={s.form}>
+        <label className={s.label}>Name</label>
+        <input
+          className={s.input}
+          onChange={input}
+          type="text"
+          name="name"
+          value={value.name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </form>
+      <form className={s.form}>
+        <label className={s.label}>Tel</label>
+        <input
+          className={s.input}
+          onChange={input}
+          type="tel"
+          name="number"
+          value={value.number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </form>
+      <button className={s.button} type="submit" onClick={submit}>
+        Add contact
+      </button>
+    </section>
+  );
+};
+
+AddForm.propTypes = {
+  input: PropTypes.func,
+  submit: PropTypes.func,
+  value: PropTypes.object,
+};
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,69 @@
+import { AddForm } from '../AddForm/AddForm';
+import { Filter } from '../Filter/Filter';
+import { ContactList } from '../ContactList/ContactList';
 import React, { Component, Fragment } from 'react';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+  inputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submit = () => {
+    const existingName = this.state.contacts.find(
+      contact => contact.name === this.state.name,
+    );
+    if (existingName) {
+      alert(`${this.state.name} existing name`);
+      return;
+    }
+    const newRecord = {
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [newRecord, ...contacts],
+    }));
+    this.setState({ name: '', number: '' });
+  };
+  updateContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
+    );
+  };
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
   render() {
     return (
       <Fragment>
-        <p>hello</p>
+        <h1>Phonebook</h1>
+        <AddForm
+          submit={this.submit}
+          input={this.inputChange}
+          value={this.state}
+        />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} input={this.inputChange} />
+        <ContactList
+          updatedContacts={this.updateContacts()}
+          deleteContact={this.deleteContact}
+        />
       </Fragment>
     );
   }
